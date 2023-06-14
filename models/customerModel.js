4dffbf91a273b0d3bdbc957286e2d32fa50c887c
MannShah17 <mannshah2004@gmail.com>
2023-06-14 15:17:11 +0530

    done

--- models/customerModel.js
+++ models/customerModel.js
@@ -1,24 +1,11 @@
 const mongoose = require('mongoose');
 
+// name: A required string representing the name of the customer.
+// contactDetails: A required string representing the contact details of the customer.
+// outstandingBalance: A number representing the outstanding balance of the customer. It has a default value of 0.
+// transactionHistory: An array of Transaction object references. It stores the transaction history of the customer.
 const customerSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  contactDetails: {
-    type: String,
-    required: true,
-  },
-  outstandingBalance: {
-    type: Number,
-    default: 0,
-  },
-  transactionHistory: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Transaction',
-    },
-  ],
+  //Write a customer Model here
 });
 
 const Customer = mongoose.model('Customer', customerSchema);
